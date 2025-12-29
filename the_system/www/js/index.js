9dc41b7c9692f1904d640cb49e3909f73d5f10ac
Felipe Silva <feasilva1212@gmail.com>
2025-12-29 17:00:14 -0300

    Adiciona botão de visualizar senha

--- the_system/www/js/index.js
+++ the_system/www/js/index.js
@@ -1,14 +1,13 @@
-const text = "Bem vindo!"
-const h1 = document.getElementById("typing");
-let index = 0;
+const inputSenha = document.getElementById("password");
+const botao = document.getElementById("togglePassword");
+const icon = document.getElementById("icon");
 
-function type() {
-    h1.innerText = text.slice(0,index);
-    index++;
-
-    if (index <= text.length) {
-        setTimeout(type, 80);
-    }
-}
-
-type();
+botao.addEventListener("click", () => {
+  if (inputSenha.type === "password") {
+    inputSenha.type = "text";
+    icon.textContent="visibility";
+  } else {
+    inputSenha.type = "password";
+    icon.textContent="visibility_off";
+  }
+});
